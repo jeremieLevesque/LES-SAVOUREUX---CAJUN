e3c0b69921805927ac7f31459f7c56b5825cea49
jeremieLevesque <70386087+jeremieLevesque@users.noreply.github.com>
2022-11-16 22:55:17 -0500

    changement de theme

---
+++ Cajun/wp-content/themes/bootstrap-basic/footer.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The theme footer
+ * 
+ * @package bootstrap-basic
+ */
+?>
+
+			</div><!--.site-content-->
+			
+			
+			<footer id="site-footer" role="contentinfo">
+			<section class="contact-area" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 offset-lg-3">
+                        <div class="contact-content text-center">
+                            <a href="#"><img src="logo_navbar.png" alt="logo"></a>
+                            <p href="https://goo.gl/maps/ag99MjikfQzcjE4dA">55 Rue Claude-Audy, Saint-Jérôme<br>info@lessavoureux.com<br>
+      450-304-0977                    </p>
+      
+                            <div class="hr"></div>
+                            
+                            
+                            <div class="contact-social">
+                                <ul>
+                                    <li><a class="hover-target" href=""><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a class="hover-target" href=""><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a class="hover-target" href=""><i class="fab fa-github"></i></a></li>
+                                    <li><a class="hover-target" href=""><i class="fab fa-behance"></i></a></li>
+                                    <li><a class="hover-target" href=""><i class="fab fa-pinterest-p"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- =============== 1.9 Contact Area End ====================-->
+        <!-- =============== 1.9 Footer Area Start ====================-->
+      
+            <p>Copyright &copy; 2022  Les Savoureux - CAJUN.</p>
+			</footer>
+		</div><!--.container page-container-->
+		
+		
+		<!--wordpress footer-->
+		<?php wp_footer(); ?> 
+	</body>
+</html>
