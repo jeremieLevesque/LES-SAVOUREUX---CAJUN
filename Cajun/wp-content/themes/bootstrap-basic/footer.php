ad6ced88c7a58e3c154e0d83a371d26fa4464abf
jeremieLevesque <70386087+jeremieLevesque@users.noreply.github.com>
2022-11-20 21:07:12 -0500

    jay

---
+++ Cajun/wp-content/themes/bootstrap-basic/footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The theme footer
+ * 
+ * @package bootstrap-basic
+ */
+?>
+
+			</div><!--.site-content-->
+			
+			
+			<footer id="site-footer" role="contentinfo" style="background-color:">
+          
+
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 offset-lg-3">
+                    <div class="contact-content text-center">
+                        <a href="/Cajun/accueil/"><img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/LOGO-LES-SAVOUREUX.png" alt="logo"></a>
+                        <a href="https://goo.gl/maps/ag99MjikfQzcjE4dA"><p>55 Rue Claude-Audy, Saint-Jérôme<br>info@lessavoureux.com<br>
+      450-304-0977                    </p></a>
+                        <div class="hr"></div>
+                    
+                        <div class="contact-social">
+                            <ul>
+                                <li><a class="hover-target" href=""><i class="fab fa-facebook-f"></i></a></li>
+                                <li><a class="hover-target" href=""><i class="fab fa-linkedin-in"></i></a></li>
+                                <li><a class="hover-target" href=""><i class="fab fa-github"></i></a></li>
+                                <li><a class="hover-target" href=""><i class="fab fa-behance"></i></a></li>
+                                <li><a class="hover-target" href=""><i class="fab fa-pinterest-p"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+ 
+    <p>Copyright &copy; 2022  Les Savoureux - CAJUN.</p>
+       
+    </footer>
+    
+			
+		</div><!--.container page-container-->
+		
+		
+		<!--wordpress footer-->
+		<?php wp_footer(); ?> 
+	</body>
+</html>
---
+++ Cajun/wp-content/themes/theme-de-base/partials/404.php
@@ -0,0 +1,15 @@
+<section>
+
+<div class="bloc-404">
+
+  <img src="https://cdn.shopify.com/s/files/1/0090/8063/5451/products/Sanstitre_32_1500x.png?v=1663768176" class="logo-404">  
+  <div class="rotation">
+  <img src="https://img2.freepng.fr/20180315/zxe/kisspng-serrano-pepper-birds-eye-chili-chili-pepper-hot-sa-pepper-png-transparent-images-5aaa4266140e54.5113058715211075580822.jpg" class="quile Bleu">
+   <img src="https://img2.freepng.fr/20180315/zxe/kisspng-serrano-pepper-birds-eye-chili-chili-pepper-hot-sa-pepper-png-transparent-images-5aaa4266140e54.5113058715211075580822.jpg" class="quile Rouge">
+  </div>  
+  <p class="texte-404">ERREUR 404</p>
+   
+</div>
+
+
+</section>
