e54dc400a3e4aa262dcabdca8c811058ef3f190c
jeremieLevesque <70386087+jeremieLevesque@users.noreply.github.com>
2022-11-22 20:04:11 -0500

    changement

--- Cajun/wp-content/themes/bootstrap-basic/functions.php
+++ Cajun/wp-content/themes/bootstrap-basic/functions.php
@@ -290,3 +290,5 @@ function bootstrapBasicGetMainColumnSize()
         wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/js/custom-gsap-scripts.js', array(), false, true );
     }
     add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
+
+    wp_enqueue_script( 'swiper-js', 'https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.5/swiper-bundle.min.js', array(), false, true );
--- Cajun/wp-content/themes/bootstrap-basic/js/main.js
+++ Cajun/wp-content/themes/bootstrap-basic/js/main.js
@@ -3,3 +3,13 @@
  * This file is for who want to make this theme as a new parent theme and you are ready to code your js here.
  */
 
+var Swipes = new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+});
