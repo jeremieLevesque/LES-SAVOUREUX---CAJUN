573aaad2cd55b98accfe90e20fbf13d278130a68
Naoufal Bensaiad <nbensaiad@hotmail.com>
2022-11-25 13:18:57 -0500

    404

--- Cajun/wp-content/themes/bootstrap-basic/404.php
+++ Cajun/wp-content/themes/bootstrap-basic/404.php
@@ -4,31 +4,22 @@
 					<main id="main" class="site-main" role="main">
 						<section class="error-404 not-found">
 							<header class="page-header">
-								<h1 class="page-title"><?php _e('Erreur 404”'); ?></h1>
+							
 							</header><!-- .page-header -->
 
 							<div class="page-content">
-								   <img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/bouteilles_sauce_trio_face-1.png" alt="" class="hero">
-        <p class="texte1">C’est quoi ça, les Savoureux? L'histoire d'une cuisine simplifiée.</p>
-        <p class="titre1">NOTRE HISTOIRE</p>
-        <p class="texte2">C’est simple! C’est une startup d’assaisonnements biologiques et sans sucre.<br><br>
-          C’est une famille qui veut cuisiner sans se casser la tête. C’est du monde vrai monde qui concocte des produits hyper-faciles à utiliser et qui goûtent le ciel. C’est nous ça Les Savoureux.<br><br>
-          
-          Notre ADN se trouve dans nos mélanges d'épices et d'herbes qui font notre renommée.<br><br>Pour ajouter au plaisir, on offre maintenant des produits alimentaires rehaussés de nos savoureuses épices. (Ex. Viandes marinées, saucisses décadentes, soupes, tofu, boulettes et d'autres beaux produits bientôt disponibles)
-          Tu ne trouves pas ces beaux produits sur notre site? C'est normal, ils sont offerts lors de ventes éphémères sur notre site web +/- aux deux mois. Pour être informé de notre prochaine vente, abonne toi à notre infolettre sur notre page d'accueil.<br><br><br><br>
-          
-          Bon appétit!<br><br>
-          
-          Martine Charbonneau - Présidente</p>
-          <div class="bloc-preparation">
-            <p class="titre2">QUI EST MARTINE?</p>
-            <p class="texte3">Diplômée du Collège des médecines douces du Québec, passionnée de cuisine depuis toujours, j’ai perfectionné mon art au fil des années me permettant de jumeler ma passion à mon travail. N’est-ce pas merveilleux?<br><br>
-              En 2012, j’ai fondé un service de traiteur. Mon but premier était de préparer des mets santé et savoureux pour les familles pressées. Ce créneau fut porteur puisqu’il facilitait la vie des gens pressés en leur donnant accès à des plats réconfortants et nutritifs dans un concept clé en main.<br><br>
-              Cette expérience m’a amenée à être sollicitée pour collaborer avec des magazines connus ainsi qu’avec des compagnies alimentaires de renommée internationale pour développer des recettes santé.<br><br>
-              En 2016 j’ai entrepris des cours au Collège des médecines douces du Québec. Je désirais additionner mes connaissances en cuisine à des connaissances académiques. Depuis, j’ai obtenu mon diplôme avec succès en Nutrithérapie et j’ai entrepris une carrière d’accompagnatrice en régime cétogène ou faible en glucides.</p>
-          </div>
-									</div>
-								</div>
+								   
+<div class="bloc-404">
+
+<img src="https://cdn.shopify.com/s/files/1/0090/8063/5451/files/Sauce_piquante_Les_Savoureux_Piri-Piri_Sriracha_Louisianaise_Hot_Sauce_1024x1024.jpg?v=1668101165" class="logo-404">  
+<div class="rotation">
+<img src="https://img2.freepng.fr/20180315/zxe/kisspng-serrano-pepper-birds-eye-chili-chili-pepper-hot-sa-pepper-png-transparent-images-5aaa4266140e54.5113058715211075580822.jpg" class="quile Bleu">
+ <img src="https://img2.freepng.fr/20180315/zxe/kisspng-serrano-pepper-birds-eye-chili-chili-pepper-hot-sa-pepper-png-transparent-images-5aaa4266140e54.5113058715211075580822.jpg" class="quile Rouge">
+</div>  
+<p class="texte-404">ERREUR 404</p>
+ 
+</div>
+
 							</div><!-- .page-content -->
 						</section><!-- .error-404 -->
 					</main>
--- Cajun/wp-content/themes/bootstrap-basic/js/main.js
+++ Cajun/wp-content/themes/bootstrap-basic/js/main.js
@@ -15,3 +15,56 @@ var Swipes = new Swiper('.swiper-container', {
 });
 
 
+gsap.timeline()
+      .fromTo('.Bleu' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"25%" ,y: '-250px', duration:2 })
+      .fromTo('.Bleu' , {rotationZ:"25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+      .fromTo('.Rouge' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"-25%" ,y: '-250px', duration:2 })
+      .fromTo('.Rouge' , {rotationZ:"-25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+      .fromTo('.Bleu' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"-25%" ,y: '-250px', duration:2 })
+      .fromTo('.Bleu' , {rotationZ:"-25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+       .fromTo('.Rouge' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"25%" ,y: '-250px', duration:2 })
+      .fromTo('.Rouge' , {rotationZ:"25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+      .fromTo('.Bleu' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"25%" ,y: '-250px', duration:2 })
+      .fromTo('.Bleu' , {rotationZ:"25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+      .fromTo('.Rouge' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"-25%" ,y: '-250px', duration:2 })
+      .fromTo('.Rouge' , {rotationZ:"-25%"},
+            { rotationZ:"0" , y: '0', duration:2 } )
+    
+  gsap.timeline()
+      .fromTo('.Bleu' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"5%" ,y: '-50px', duration:2 })
+      .fromTo('.Bleu' , {rotationZ:"5%", y: '-50px'},
+            { rotationZ:"0" , y: '0', duration:2 } )
+      .fromTo('.Bleu' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"-5%" ,y: '50px', duration:2 })
+      .fromTo('.Bleu' , {rotationZ:"-5%" ,y: '50px'},
+            { rotationZ:"0" , y: '0', duration:2 } )    
+gsap.timeline()
+      .fromTo('.Rouge' , {rotationZ:"0%", y: '0%'},
+            { rotationZ:"5%" ,y: '-50px', duration:2 })
+      .fromTo('.Rouge' , {rotationZ:"5%", y: '-50px'},
+            { rotationZ:"0" , y: '-50px', duration:2 } )
+      .fromTo('.Rouge' , {rotationZ:"0%", y: '-50px'},
+            { rotationZ:"-5%" ,y: '-50px', duration:2 })
+      .fromTo('.Rouge' , {rotationZ:"-5%" ,y: '-50px'},
+            { rotationZ:"0" , y: '0', duration:2 } )    
+
+const btn = document.querySelector('.buttonlogo');
+
+  btn.addEventListener("click", function() {
+  gsap.timeline()
+      .fromTo('.buttonlogo' , {rotationZ:"0%"},
+            { rotationZ:"270%", duration:2 })
+});
+
