837716c83b298fbeb80771cbd423593f5e9cdc31
Naoufal Bensaiad <nbensaiad@hotmail.com>
2022-12-02 22:29:16 -0500

    footer

--- Cajun/wp-content/themes/bootstrap-basic/footer.php
+++ Cajun/wp-content/themes/bootstrap-basic/footer.php
@@ -17,30 +17,16 @@
                 <div class="col-lg-12 offset-lg-3">
                     <div class="contact-content text-center">
                         <a href="/Cajun/accueil/"><img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/LOGO-LES-SAVOUREUX.png" alt="logo"></a>
-                        <div class="pied-de-page">
-                        <div class="container">
-  <h2>Les Partenaires</h2>
-   <section class="customer-logos slider">
-      <div class="slide"><img src="https://static.lpcdn.ca/lpweb/mobile/img/la-presse-logo-web.svg"></div>
-      <div class="slide"><img src="https://www.coupdepouce.com/assets/img/logo.png?v=1522262616"></div>
-      <div class="slide"><img src="https://tse2.mm.bing.net/th/id/OIP.F3avmaRa33aKuFwvYDp-EgHaD8?pid=ImgDet&rs=1"></div>
-      <div class="slide"><img src="https://tse3.mm.bing.net/th/id/OIP.5njq7tj2O5aIH9NJSBw8yAHaEK?pid=ImgDet&rs=1"></div>
-      <div class="slide"><img src="https://tse4.mm.bing.net/th/id/OIP.WxOJe_QHhqvLVYTG_n0y3gHaHa?pid=ImgDet&rs=1"></div>
-      <div class="slide"><img src="https://tse3.mm.bing.net/th/id/OIP.M2zUCRDVRKVEFlMtAGKpMQAAAA?pid=ImgDet&rs=1"></div>
-     
-     
-   </section>
-</div>
                         <a href="https://goo.gl/maps/ag99MjikfQzcjE4dA"><p>55 Rue Claude-Audy, Saint-Jérôme<br>info@lessavoureux.com<br>
       450-304-0977                    </p></a>
                         <div class="hr"></div>
                     
                         <div class="contact-social">
                             <ul>
-                                <li><a class="hover-target"  href="https://www.facebook.com/LesSavoureux/"><i class="fab fa-facebook-f"> <img src="http://localhost:81/Cajun/wp-content/uploads/2022/12/MicrosoftTeams-image-1.png" alt="Facebook" /></i></a></li>
-                                <li><a class="hover-target" href="https://www.instagram.com/lessavoureuxassaisonnements/"><i class="fab fa-instagram"></i></a></li>
-                                <li><a class="hover-target" href=""><i class="fab fa-github"></i></a></li>
-                            
+                                <li><a class="hover-target" href="https://www.facebook.com/LesSavoureux/"><img src="http://localhost:81/Cajun/wp-content/uploads/2022/12/MicrosoftTeams-image-1.png" class="foot-img"></i></a></li>
+                                <li><a class="hover-target" href="https://www.instagram.com/lessavoureuxassaisonnements/"><img src="http://localhost:81/Cajun/wp-content/uploads/2022/12/MicrosoftTeams-image-2-1.png" class="foot-img"></i></a></li>
+                                <li><a class="hover-target" href=""><img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/cropped-favicon-e1670034851258.png" class="foot-img"></i></a></li>
+                                
                             </ul>
                         </div>
                     </div>
@@ -55,9 +41,7 @@
 			
 		</div><!--.container page-container-->
 		<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
-        <script src="https://code.jquery.com/jquery-2.2.0.min.js" type="text/javascript"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
 		 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js"></script>
 		<!--wordpress footer-->
 		<?php wp_footer(); ?> 
