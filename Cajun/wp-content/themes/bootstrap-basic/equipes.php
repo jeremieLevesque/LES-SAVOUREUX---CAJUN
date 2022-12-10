865842fe761c84710c73cfa9fc2be5828e0205f5
Jérémie Lévesque <70386087+jeremieLevesque@users.noreply.github.com>
2022-12-10 14:04:58 -0500

    avancement catégorie

---
+++ Cajun/wp-content/themes/bootstrap-basic/equipes.php
@@ -0,0 +1,45 @@
+<?php 
+/**
+ * Template Name: equipe
+ * 
+ * @package bootstrap-basic
+ */
+
+get_header();
+
+/**
+ * determine main column size from actived sidebar
+ */
+$main_column_size = bootstrapBasicGetMainColumnSize();
+?>
+<?php get_sidebar('left'); ?> 
+				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
+					<main id="main" class="site-main" role="main">
+							<div class='main-equipe'>
+								<div class='equipe-wrapper'>
+						<?php if (have_posts()) {$membre = new WP_Query('post_type=membre'); ?> 
+						<?php 
+						
+						// start the loop
+						while ($membre->have_posts()) {
+							$membre->the_post();
+							
+							/* 
+							* Include the Post-Format-specific template for the content.
+							* If you want to override this in a child theme, then include a file
+							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+							*/
+							get_template_part('content', get_post_format());
+						}// end while
+						
+						bootstrapBasicPagination();
+						?> 
+						<?php } else { ?> 
+						<?php get_template_part('no-results', 'index'); ?>
+						<?php } // endif; ?> 
+						</div>
+						</div>
+					</main>
+				</div>
+<?php get_sidebar('right'); ?> 
+<?php get_footer(); ?> 
