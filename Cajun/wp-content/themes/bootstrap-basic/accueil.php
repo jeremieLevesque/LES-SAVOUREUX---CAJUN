d8beb7aa4dbeb7269dda0ef360280a0763e41e89
Jérémie Lévesque <70386087+jeremieLevesque@users.noreply.github.com>
2022-12-14 21:57:27 -0500

    HUB ET ACCUEIL

---
+++ Cajun/wp-content/themes/bootstrap-basic/accueil.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+
+ * The main template file
+
+ * Template Name: accueil
+
+ * @package bootstrap-basic
+
+ */
+
+
+
+get_header();
+
+
+
+/**
+
+ * determine main column size from actived sidebar
+
+ */
+
+$main_column_size = bootstrapBasicGetMainColumnSize();
+$carte_nouvelle = new WP_Query('post_type=nouvelle&showposts=3');
+?>
+
+
+                <div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
+
+                    <main id="main" class="site-main" role="main">
+<article>
+                   
+<div class="hero">
+  <div class="swiper-container">
+    
+    <!-- swiper slides -->
+    <div class="swiper-wrapper">
+        <div class="swiper-slide" style="background-image: url(http://localhost:81/Cajun/wp-content/uploads/2022/12/banniere_rouge_sans_logo.jpg);">
+            <h2>Saveurs uniques</h2>
+        </div>
+        
+        <div class="swiper-slide" style="background-image: url(http://localhost:81/Cajun/wp-content/uploads/2022/11/recette_beurre_citrouille.jpg);"> 
+            <h2>Recettes</h2>
+        </div>
+        
+        <div class="swiper-slide" style="background-image: url(http://localhost:81/Cajun/wp-content/uploads/2022/11/recette_burrata_sauce.jpg);">
+            <h2>Sauces et épices</h2>
+        </div>
+        
+     
+    </div>
+    <!-- !swiper slides -->
+    
+    <!-- next / prev arrows -->
+    <div class="swiper-button-next"></div>
+    <div class="swiper-button-prev"></div>
+    <!-- !next / prev arrows -->
+    
+    <!-- pagination dots -->
+    <div class="swiper-pagination"></div>
+    <!-- !pagination dots -->
+</div>
+</div>
+   <div class="entête">
+    <h1 class="display-4 font-weight-bold">Nouvelles</h1>
+</div>
+    
+  
+
+<!-- For demo purpose -->
+
+
+ <section class="Services">  
+ <div class="services-wrapper"> 
+						<div class="services-cartes">
+							<a style="text-decoration: none" href="http://localhost:81/Cajun/2022/12/01/service-1">
+							<p class="services-intertitre">SAUCES</p>
+							<img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/bouteilles_sauce_trio_face-1.png" alt="" class="service1">
+							<p class="services-description">Ces 3 créations sont le fruit d'une belle collaboration avec le Domaine Britan Mills, un producteur québécois récipiendaire d'une médaille...</p>
+							</a>
+						</div>
+
+						<div class="services-cartes">
+							<a style="text-decoration: none" href="http://localhost:81/Cajun/2022/12/01/service-2">
+							<p class="services-intertitre">ÉPICES</p>
+							<img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/pot_epices_bleu.png" alt="" class="service2">
+							<p class="services-description">Une savante combinaison de notes exotiques qui s'accordent dans tous vos sautés avec poulet, crevettes, porc ou bœuf. Aussi délicieux dans une soupe...</p>
+							</a>
+						</div>
+
+						<div class="services-cartes">
+							<a style="text-decoration: none" href="http://localhost:81/Cajun/2022/12/01/service-3">
+							<p class="services-intertitre">BOÎTES</p>
+							<img src="http://localhost:81/Cajun/wp-content/uploads/2022/11/boite_cadeau_jaune_transparent.png" alt="" class="service3">
+							<p class="services-description">Notre assaisonnement BBQ, le roi de nos classiques! Pourquoi essayer notre mélange d’épices? Il permet une cuisine simple et rapide...</p>
+							</a>
+						</div>
+						</div>
+</section>
+
+  <section class="Nouvelles">
+  <div class="main-services">
+					<div class="services-wrapper"> 
+                    <?php if ($carte_nouvelle->have_posts()) : while ($carte_nouvelle->have_posts()) : $carte_nouvelle->the_post();  ?>
+
+                       
+						
+
+                        
+					
+						<div class="services-cartes">
+							<a  href="<?php the_field('lien_recette'); ?>" style="text-decoration: none">
+							<p class="services-intertitre"><?php the_field('titre-viande'); ?><?php the_field('titre-vegan'); ?></p>
+							<img src="<?php the_field('image'); ?>" class="service1">
+							<p class="services-description"><?php the_field('resume'); ?></p>
+							</a>
+						</div>
+
+						
+
+
+<?php
+	endwhile;
+endif;
+wp_reset_query();
+?>
+			</div>
+						</div>	
+</section>
+
+
+
+
+
+         <section class="bloc-temoignage">
+              <div class="heroindexfirst">
+                <p class=" font-weight-bold lead mb-0 color">TÉMOIGNAGES</p>
+                <div class="temoignage-container">
+                  <div class="hero__1">
+                    <img src="https://thispersondoesnotexist.com/image" alt="" class="hero--image tem-img">
+                      <div class="hero--bio">
+                      <h1>quelqu'un</h1>
+                     <h3>Propriétaire</h3>
+                      <blockquote>Dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<cite>Lorem Ipsum</cite></blockquote>
+                    </div>
+                  </div>
+                  
+                
+                </div>
+                  </div>
+</section>
+
+
+					
+						
+</article>
+                    </main>
+
+                </div>
+
+
+<?php get_footer(); ?>
+
--- Cajun/wp-content/themes/bootstrap-basic/hub_news.php
+++ Cajun/wp-content/themes/bootstrap-basic/hub_news.php
@@ -1,55 +1,85 @@
 <?php
+
 /**
+
  * The main template file
+
  * Template Name: hub-news
+
  * @package bootstrap-basic
+
  */
+
+
 
 get_header();
 
-/**
- * determine main column size from actived sidebar
- */
-$main_column_size = bootstrapBasicGetMainColumnSize();
-?>
-<?php get_sidebar('left'); ?> 
-				<div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
 
-					<main id="main" class="site-main" role="main">
-					<article>
-					<div id="app">
-      					<hero srch="http://localhost:81/Cajun/wp-content/uploads/2022/11/personne_martine_charbonneau_pots_epices.jpg" titre="NOUVELLES">
-      					</hero>
-					<description txt1="On te prépare ça le jour même, et ça part le lendemain (sauf si tu commandes le vendredi à 17 h. Là, ça partira peut-être le lundi, surtout si l’envie d’un verre de vin est très pressante!) Ensuite, ça prend environ 1 à 3 jours ouvrables à nos transporteurs pour te mettre ça dans ta boite aux lettres!">
-     				</description>
+
+/**
+
+ * determine main column size from actived sidebar
+
+ */
+
+$main_column_size = bootstrapBasicGetMainColumnSize();
+$carte_nouvelle = new WP_Query('post_type=nouvelle&showposts=6&category_name=vegan');
+?>
+
+
+                <div class="col-md-<?php echo $main_column_size; ?> content-area" id="main-column">
+
+                    <main id="main" class="site-main" role="main">
+<article>
+                   
+                    <div id="app">
+
+                        <hero srch="http://localhost:81/Cajun/wp-content/uploads/2022/11/personne_martine_charbonneau_pots_epices.jpg" titre="NOUVELLES">
+
+                        </hero>
+
+                    <description txt1="On te prépare ça le jour même, et ça part le lendemain (sauf si tu commandes le vendredi à 17 h. Là, ça partira peut-être le lundi, surtout si l’envie d’un verre de vin est très pressante!) Ensuite, ça prend environ 1 à 3 jours ouvrables à nos transporteurs pour te mettre ça dans ta boite aux lettres!">
+
+                    </description>
 					</div>
-					<?php if (have_posts()) {$carte_nouvelle = new WP_Query('post_type=nouvelle'); ?> 
-						<?php 
-						// start the loop
-						while ($carte_nouvelle->have_posts()) {
-							$carte_nouvelle->the_post();
-							
-							?><?php
-							
-							/* 
-							* Include the Post-Format-specific template for the content.
-							* If you want to override this in a child theme, then include a file
-							* called content-___.php (where ___ is the Post Format name) and that will be used instead.
-							*/
-							get_template_part('content', get_post_format());
-						}// end while
+
+
+					<div class="main-services">
+					<div class="services-wrapper"> 
+                    <?php if ($carte_nouvelle->have_posts()) : while ($carte_nouvelle->have_posts()) : $carte_nouvelle->the_post();  ?>
+
+                       
 						
-						bootstrapBasicPagination();
-						?> 
-						<?php } else { ?> 
-						<?php get_template_part('no-results', 'index'); ?>
-						
-						<?php } // endif; ?> 
-						<button class="bouton-suite">PLUS DE NOUVELLES</button>
+
+                        
 					
-						</article>
-					</main>
-				</div>
-<?php get_sidebar('right'); ?> 
-<?php get_footer(); ?> 
+						<div class="services-cartes">
+							<a  href="<?php the_field('lien_recette'); ?>" style="text-decoration: none">
+							<p class="services-intertitre"><?php the_field('titre-viande'); ?><?php the_field('titre-vegan'); ?></p>
+							<img src="<?php the_field('image'); ?>" class="service1">
+							<p class="services-description"><?php the_field('resume'); ?></p>
+							</a>
+						</div>
+
+						
+
+
+<?php
+	endwhile;
+endif;
+wp_reset_query();
+?>
+			</div>
+						</div>		
+					
+<a href="http://localhost:81/Cajun/recette2/?preview_id=1133&preview_nonce=66919480f4&preview=true" style="text-decoration: none"><button class="bouton-suite">Page suivante</button></a>
+
+		
+</article>
+                    </main>
+
+                </div>
+
+
+<?php get_footer(); ?>
 
